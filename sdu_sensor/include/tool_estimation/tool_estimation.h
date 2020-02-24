fcfd6493607f58d6d13dfcbd97c5d208fc4be179
robotemperor <kimpd1224@gmail.com>
2020-02-24 16:48:02 +0100

    torque model added

--- sdu_sensor/include/tool_estimation/tool_estimation.h
+++ sdu_sensor/include/tool_estimation/tool_estimation.h
@@ -27,10 +27,12 @@ class ToolEstimation
     void offset_init(Eigen::MatrixXd data, int desired_sample_num);
     void set_parameters(double control_time_init, double mass_of_tool_init);
     void set_acc_input_data(Eigen::MatrixXd linear_acc_input);
+    void set_pose_input_data(Eigen::MatrixXd pose);
+
+
     Eigen::MatrixXd get_offset_data();
     Eigen::MatrixXd get_angular_acc();
     Eigen::MatrixXd get_one_axis_inertia_tensor(Eigen::MatrixXd ft_data, std::string axis);
-
     Eigen::MatrixXd get_contacted_force(Eigen::MatrixXd ft_data, Eigen::MatrixXd linear_acc_data); // 6*1 data fx fy fz tx ty tz
 
     //filter
@@ -38,14 +40,30 @@ class ToolEstimation
 
     std::shared_ptr<KalmanFilter> kf_accelerometer;
 
+    std::shared_ptr<Kinematics> tool_kinematics;
 
+    std::shared_ptr<Statistics> tool_statistics_orientation_vel;
+
+    std::shared_ptr<Statistics> tool_statistics_orientation_acc;
+
+    Eigen::MatrixXd orientation_;
+
+    Eigen::MatrixXd pose_;
+
+    Eigen::MatrixXd pose_vel_;
+    Eigen::MatrixXd orientation_vel_;
+
+    Eigen::MatrixXd pose_acc_;
+    Eigen::MatrixXd orientation_acc_;
+
+
+    Eigen::MatrixXd inertia_of_tool_;
 
 
   private:
     double control_time_;
     double mass_of_tool_;
 
-    Eigen::MatrixXd inertia_of_tool_;
 
     Eigen::MatrixXd tool_linear_acc_data_;
     Eigen::MatrixXd tool_linear_acc_offset_data_;
